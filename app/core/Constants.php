59989eb162523dec7506b080d551b33194829c07
winarto <winarto_xyz@hotmail.com>
2020-03-30 15:53:50 +0700

    menambahkan asset

---
+++ app/core/Constants.php
@@ -0,0 +1,2 @@
+<?php
+define('BASEURL', 'http://localhost:99/phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-    <h1>Halaman About Index</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/winter.png" alt="Winter" width="150" class="rounded-circle shadow">
     <p>Hallo nama : <?= $data['nama'] ?>, umur : <?= $data['umur'] ?>, pekerjaan : <?= $data['pekerjaan'] ?></p>
+</div>
+    
