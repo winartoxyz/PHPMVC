c507f38775d01bed4431c24d92450cda9092da20
winarto <winarto_xyz@hotmail.com>
2020-03-31 16:17:28 +0700

    Searching Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,18 +1,32 @@
 <div class="container mt-4">
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
             <?php
                 Flasher::flash();
             ?>
         </div>
     </div>
-    <div class="row">
-        <div class="col-6">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
+    <div class="row mb-3">
+        <div class="col-lg-6 ">
+           <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
                 Tambah Mahasiswa
             </button>
-            <br/><br/>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-lg-6 ">
+          <form action="<?= BASEURL ?>/mahasiswa/cari" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="cari mahasiswa" aria-describedby="button-addon2" name="keyword" id="keyword" autocomplete="off">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+                </div>
+            </div>
+          </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
             <h3>Daftar Mahasiswa</h3>
                 <ul class="list-group">
                     <?php foreach($data['mhs'] as $mhs) : ?>     
